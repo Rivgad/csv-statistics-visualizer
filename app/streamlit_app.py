48159fa63dfbfe45ca25035bd2cc6a8a624905e1
Rivgad <losvermo@gmail.com>
2023-08-05 18:55:08 +0300

    formatting code

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -4,23 +4,12 @@ import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
+with st.echo(code_location="below"):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
+    Point = namedtuple("Point", "x y")
     data = []
 
     points_per_turn = total_points / num_turns
@@ -33,6 +22,8 @@ with st.echo(code_location='below'):
         y = radius * math.sin(angle)
         data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    st.altair_chart(
+        alt.Chart(pd.DataFrame(data), height=500, width=500)
+        .mark_circle(color="#0068c9", opacity=0.5)
+        .encode(x="x:Q", y="y:Q")
+    )
