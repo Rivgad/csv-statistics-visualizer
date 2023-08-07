66257664ee743b653e1bddcb31b29f0352662d89
Yusupov Adel <losvermo@gmail.com>
2023-08-07 16:28:00 +0300

    multiselects and try-except
    * add multiselections for columns and algorithms
    
    * add catch for exceptions

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+from pandas.errors import ParserError
 
 
 def main():
@@ -11,7 +12,45 @@ def main():
         if uploaded_file is not None:
             dataframe = pd.read_csv(uploaded_file)
             st.write(dataframe[:5])
+        else:
+            return
+
+        columns = dataframe.columns.tolist()
+        columns_options = st.multiselect(
+            "Choose columns",
+            options=columns,
+            max_selections=2,
+            placeholder="Choose two columns",
+        )
+
+        if len(columns_options) != 2:
+            st.warning("You have to select 2 columns")
+            return
+
+        testing_algorithms_options = st.multiselect(
+            "Choose algorithms",
+            options=[
+                "A/B test",
+                "t-test",
+                "p-value",
+                "u-test",
+                "bootstraping",
+                "chi-square",
+            ],
+            placeholder="Choose at least 2 algorithms",
+        )
+
+        if len(testing_algorithms_options) < 2:
+            st.warning("You have to choose at least 2 algorithms")
+            return
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except ParserError as ex:
+        st.error(
+            "An error occurred while trying to parse the dataframe. Check the file and try again."
+        )
+    except Exception as ex:
+        st.exception(ex)
