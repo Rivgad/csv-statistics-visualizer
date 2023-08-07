c15cade2b3f05cd5f2d6f370b3c8b2c2b394b2da
Rivgad <losvermo@gmail.com>
2023-08-07 16:25:57 +0300

    add multiselections for columns and algorithms

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -11,6 +11,37 @@ def main():
         if uploaded_file is not None:
             dataframe = pd.read_csv(uploaded_file)
             st.write(dataframe[:5])
+        else:
+            return
+
+        columns = dataframe.columns.tolist()
+        columns_options = st.multiselect(
+            "Choose columns",
+            options=columns,
+            max_selections=2,
+            placeholder="Choose two columns",
+        )
+
+        if len(columns_options) != 2:
+            st.warning("You have to select 2 columns")
+            return
+
+        testing_algorithms_options = st.multiselect(
+            "Choose algorithms",
+            options=[
+                "A/B test",
+                "t-test",
+                "p-value",
+                "u-test",
+                "bootstraping",
+                "chi-square",
+            ],
+            placeholder="Choose at least 2 algorithms",
+        )
+
+        if len(testing_algorithms_options) < 2:
+            st.warning("You have to choose at least 2 algorithms")
+            return
 
 
 if __name__ == "__main__":
